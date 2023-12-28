afeba9e93ea1982367fa65d9a9e5e20f41cb761e
kirnjiyun <kimjiyunee@naver.com>
2023-12-28 16:35:40 +0900

    페이지 라우팅하기

--- 1228/src/App.js
+++ 1228/src/App.js
@@ -1,8 +1,17 @@
-function App() {
-  return (
-    <div>
-      hello world
-    </div>
-  );
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Search from "./pages/Search";
+import Country from "./pages/Country";
+import NotFound from "./pages/NotFound";
+
+export default function App() {
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/country" element={<Country />} />
+            <Route path="*" element={<NotFound />} />
+        </Routes>
+    );
 }
-export default App;
