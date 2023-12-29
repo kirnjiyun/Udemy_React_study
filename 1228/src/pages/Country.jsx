47e41cc0383e43d669d4a084aea0c741e289795f
kirnjiyun <kimjiyunee@naver.com>
2023-12-29 16:14:35 +0900

    api

--- 1228/src/pages/Country.jsx
+++ 1228/src/pages/Country.jsx
@@ -1,7 +1,19 @@
-import React from "react";
 import { useParams } from "react-router-dom";
+import { fetchCountry } from "../api";
+import { useEffect, useState } from "react";
+
 export default function Country() {
-    const params = useParams();
-    console.log(params);
-    return <div>Country :{params.code}</div>;
+    const pararms = useParams();
+    const [country, setCountry] = useState();
+
+    const setInitData = async () => {
+        const data = await fetchCountry(pararms.code);
+        setCountry(data);
+    };
+
+    useEffect(() => {
+        setInitData();
+    }, [pararms.code]);
+
+    return <div>Country : {pararms.code}</div>;
 }
--- 1228/src/pages/Home.jsx
+++ 1228/src/pages/Home.jsx
@@ -1,5 +1,17 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { fetchCountries } from "../api";
 
 export default function Home() {
+    const [countries, setCountries] = useState([]);
+
+    const setInitData = async () => {
+        const data = await fetchCountries();
+        setCountries(data);
+    };
+
+    useEffect(() => {
+        setInitData();
+    }, []);
+
     return <div>Home</div>;
 }
